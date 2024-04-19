a33288a741f580fee03d33036522fac51a3ec554
Anton Permiakov <anton.permiakov@idt.net>
2024-04-19 17:09:30 +0300

    move scripts to body

--- home/loader.js
+++ home/loader.js
@@ -22,8 +22,8 @@ window.onload = function() {
     legacyScript.crossOrigin = true
     legacyScript.dataset.src = appUrl + 'index-legacy.js?v=' + version;
 
-    document.head.appendChild(styles);
-    document.head.appendChild(script);
-    document.head.appendChild(legacyPolyfill);
-    document.head.appendChild(legacyScript);
+    document.body.appendChild(styles);
+    document.body.appendChild(script);
+    document.body.appendChild(legacyPolyfill);
+    document.body.appendChild(legacyScript);
 }
